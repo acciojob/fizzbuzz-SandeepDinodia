f138001761524bfe9457babb7a02e85642a1319b
SandeepDinodia <151106767+SandeepDinodia@users.noreply.github.com>
2024-05-21 18:12:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-//your JS code here. If required.
-for(let i=1; i<=20; i++){
+// //your JS code here. If required.
+// for(let i=1; i<=20; i++){
+// 	if(i%3==0){
+// 		console.log("Fizz");
+// 	}else if(i%5==0){
+// 		console.log("Buzz");
+// 	}
+// 	else if(i%3==0 && i%5==0){
+// 		console.log("FizzBuzz");
+// 	}
+// 	else {
+// 		console.log(i);
+// 	}
+// }
+for(let i =0; i<20; i++){
 	if(i%3==0){
 		console.log("Fizz");
 	}else if(i%5==0){
@@ -7,8 +20,5 @@ for(let i=1; i<=20; i++){
 	}
 	else if(i%3==0 && i%5==0){
 		console.log("FizzBuzz");
-	}
-	else {
-		console.log(i);
 	}
 }
